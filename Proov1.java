de1d05ea0cf9af4b7489839e5709a65e01f6d7d7
vladsuto <vladsuto@greeny.cs.tlu.ee>
2017-02-16 08:23:07 +0200

    Added examples #2

--- Proov1.java
+++ Proov1.java
@@ -4,3 +4,14 @@ public class Proov1{
 		System.out.println(randomnumbergenerator1.toString());
 	}
 }
+
+
+/*
+
+Sisestatud arv oli 512, random arv oli 262, see tahendab et: true
+Sisestatud arv oli 512, random arv oli 2, see tahendab et: false
+Sisestatud arv oli 512, random arv oli 502, see tahendab et: true
+Sisestatud arv oli 512, random arv oli 412, see tahendab et: true
+
+
+*/
